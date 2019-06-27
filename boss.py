2ec6d58d0c98f41b40876c052bec40f5cb69122c
Qing Jie <jie.qing@afconsult.com>
2019-06-27 17:36:34 +0800

    add xlsx

---
+++ boss.py
@@ -0,0 +1,39 @@
+# -*-coding:utf-8 -*-
+
+DaSaluo_BossList = [
+    {"Name": "圣光勇士", "ID": 2265},
+    {"Name": "猿猴", "ID": 2263},
+    {"Name": "玉火大师", "ID": 2266},
+
+    {"Name": "风灵", "ID": 2271},
+
+    {"Name": "神选着", "ID": 2268},
+
+
+    {"Name": "大王", "ID": 2272},
+
+    {"Name": "大工匠", "ID": 2276},
+
+    {"Name": "风墙", "ID": 2280},
+
+    {"Name": "吉安娜", "ID": 2281}
+ 
+]
+
+YongHengWangGong_BossList=[
+    
+    {"Name": "BOSS1", "ID": 2298},
+    
+    {"Name": "BOSS2", "ID": 2305},
+    
+    {"Name": "BOSS3", "ID": 2289},
+    
+    {"Name": "BOSS4", "ID": 2304},
+    
+    {"Name": "555", "ID": 2303},
+    
+    {"Name": "666", "ID": 2311},
+    {"Name": "777", "ID": 2239},
+    {"Name": "女王", "ID": 2299}
+
+]
